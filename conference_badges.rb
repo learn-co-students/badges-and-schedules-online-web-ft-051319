fdcc32117fb91203902030d8942ee0a2aa2f9c36
poignant-minifier-0983 <davidfano@gmail.com>
2019-05-13 20:10:04 +0000

    Done.

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,17 @@
-# Write your code here.
+def badge_maker(name)
+  "Hello, my name is #{name}."
+end
+
+def batch_badge_creator(name_list)
+  name_list.map do |name|
+    badge_maker(name)
+  end
+end
+
+def assign_rooms(list)
+	new_arr = []
+	list.each_with_index do |name,index|
+		new_arr << "Hello, #{name}! You'll be assigned to room #{index+1}!"
+	end
+	print new_arr
+end
